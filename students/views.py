b435367162b1dc6a81b94b5ad81c83e404cd2369
andreisovidiu <andreisovidiu@gmail.com>
2024-05-01 16:13:44 +0200

    register template

--- students/views.py
+++ students/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
 
 def register(request):
-    return render(request, "students/register.html")
-
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            return redirect('composerindex')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'students/register.html', {'form': form})
+    
 def login(request):
     return render(request, "students/login.html")
