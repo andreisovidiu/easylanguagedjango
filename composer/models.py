7ed9aac46289802a6f0d6d20d0f3800b394bcc82
Andrei <andreisovidiu@gmail.com>
2024-05-27 21:33:17 +0200

    gpt output

--- composer/models.py
+++ composer/models.py
@@ -2,18 +2,4 @@ from django.db import models
 from django.template.loader import render_to_string
 
 class Strategy(models.Model):
-    template = 'composer/strategy.html'
-
-    rendered = models.TextField(('Rendered strategy'), blank=True, default='')
-
-    def save(self, *args, **kwargs):
-        if not self.rendered:
-            self.rendered = render_to_string(self.template, self.context)
-        return super().save(*args, **kwargs) 
-
-
-
-
-
-
-    
+    pass
--- composer/views.py
+++ composer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Strategy
 
@@ -31,7 +31,9 @@ def index(request):
                 "tradeliquid": tradeliquid
                 })
         elif request.POST['button'] == 'Save':
-            pass # Save into model
+            strategy = Strategy.create_from_request(request)
+            strategy.save()
+            return redirect('composerindex')
     else:
         return render(request, "composer/index.html")
 
